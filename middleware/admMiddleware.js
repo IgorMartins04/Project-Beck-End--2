110ace65d3a1017292e5d859cdc6f1736fae72a3
Igor <163761078+IgorMartins04@users.noreply.github.com>
2025-02-17 16:21:46 -0300

    Update admMiddleware.js

--- middleware/admMiddleware.js
+++ middleware/admMiddleware.js
@@ -12,14 +12,14 @@ module.exports = (req, res, next) => {
   };
   
   const isAdmin = (req, res, next) => {
-    // Verifica se o usuário está autenticado e possui o campo 'regra'
+    
     if (!req.user || req.user.regra !== "admin") {
       return res.status(403).json({
         mensagem: "Apenas administradores podem gerenciar ingressos.",
       });
     }
   
-    next(); // Usuário é administrador, prossegue para a próxima função
+    next(); 
   };
   
   module.exports = isAdmin;
